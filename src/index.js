18264ea05b7f8e82a2c6da3978548cdfcad24b2e
EssenceCode <123456789+odin@users.noreply.github.com>
2023-09-05 11:04:20 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import Projects from './project.js';
-import Todo from './todo.js';
 import View from './view.js'; 
-// import ProjectStorage from './storage.project.js';
 import './style.css'
 
 
-const todo = Todo('todo1')
 
-const project = Projects('project1')
-
-project.addTodo(todo)
 
 
 
@@ -22,5 +15,5 @@ View().ProjectDelete()
 View().TodoRender()
 View().TodoFormButton()
 
-// ProjectStorage()
+
 
